014b7ece90542b2d62f2399384c1a63ea9971419
Hongsu Wang <sudospace@gmail.com>
2018-12-05 04:28:59 +0800

    create step by step notes

--- parser.py
+++ parser.py
@@ -110,12 +110,17 @@ def writeFile(dataDic, fileName):
         f.write("\n".join(output))
 
 
-# Read DDBC data（open html file by browser, and then CtrlA to get all the data）
-# Please detele the paragraphs at the top of the page, Ex:
+
+# Step 1, create input.txt:
+# 1. Read DDBC html file by browser. 2. CtrlA to get all the data. 3. copy and paste that data to input.txt
+#
+# Step 2, please detele the paragraphs at the top of the page, Ex:
 # *Z碼系統使用GRS67經緯度座標系統，與GPS或Google Earth使用WGS84經緯度座標系統略有不同，有需要可利用「Web版坐標轉換程式」進行座標轉換
 #
 # 檢索【 】(地名：257 筆，群組：0 筆) ：
 # 。朝代過濾器(不含群組)：
+# Step 3, run parser.py. My environment is python3.5
+
 inputDataList = readfile("input.txt")
 
 # Convert the raw data to a dictionary, use 規範碼 as the index
